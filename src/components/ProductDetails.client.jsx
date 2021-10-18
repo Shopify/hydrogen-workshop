0f4b2d3c9780cae0622645700f3d37446c6c4378
cathryngriffiths <cathryn.griffiths@shopify.com>
2021-10-18 15:40:53 -0400

    Some more product page updates

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -12,18 +12,34 @@ export default function ProductDetails({ product }) {
         </div>
         <div>
           <Product.Title className="font-bold text-3xl" />
-          <div className="space-x-2 text-xl font-semibold my-4">
-            <Product.SelectedVariant.Price as="span" />
+          <div className="space-x-2 text-xl my-4 flex items-center">
+            <Product.SelectedVariant.Price as="div">
+              {(money) => {
+                return (
+                  <div className="flex items-center">
+                    <span className="rounded-lg text-xs bg-black text-white px-2 py-1 mr-2">{money.currencyCode}</span>
+                    <span>{money.currencyNarrowSymbol}</span>
+                    <span>{money.parts.map((part) => part.value).join('')}</span>
+                  </div>
+                )
+              }}
+              </Product.SelectedVariant.Price>
             <Product.SelectedVariant.Price
               priceType="compareAt"
-              className="line-through text-gray-400"
+              className="line-through text-gray-400 text-sm"
               as="span"
             />
           </div>
           <Options />
-          <Product.SelectedVariant.AddToCartButton className="rounded-lg bg-black text-white px-10 py-2 font-semibold my-4">
-            Add to cart
-          </Product.SelectedVariant.AddToCartButton>
+          <div className="space-x-2">
+            <Product.SelectedVariant.AddToCartButton className="rounded-lg bg-black text-white px-10 py-2 font-semibold my-4">
+              Add to cart
+            </Product.SelectedVariant.AddToCartButton>
+            <Product.SelectedVariant.BuyNowButton className="rounded-lg border border-black px-10 py-2 font-semibold my-4">
+              Buy it now
+            </Product.SelectedVariant.BuyNowButton>
+          </div>
+          <Product.SelectedVariant.ShopPayButton />
           <Product.Description />
         </div>
       </div>
@@ -35,7 +51,7 @@ function Media() {
   const { media } = useProduct();
 
   return (
-    <ul className="mt-2 grid grid-cols-2 gap-2">
+    <ul className="mt-2 grid grid-cols-4 gap-2">
       {media.map((mediaFile) => {
         return (
           <li>
