20726f1b22cac45071268446cd5cb58e370fa135
cathryngriffiths <cathryn.griffiths@shopify.com>
2021-10-21 14:13:13 -0400

    Added Media

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -1,3 +1,42 @@
+import {Product, useProduct, MediaFile} from '@shopify/hydrogen/client';
+
 export default function ProductDetails({product}) {
-  return <p>Hello from ProductDetails, {product.title}</p>
+  const initialVariant = product.variants.edges[0].node;
+
+  return (
+    <Product product={product} initialVariantId={initialVariant.id}>
+      <Product.SelectedVariant.Image />
+      <Gallery />
+      <Product.Title />
+      <Product.SelectedVariant.Price />
+      <Product.SelectedVariant.Price priceType="compareAt"/>
+      <Product.SelectedVariant.AddToCartButton>
+        Add to cart
+      </Product.SelectedVariant.AddToCartButton>
+      <Product.SelectedVariant.BuyNowButton>
+        Buy it now
+      </Product.SelectedVariant.BuyNowButton>
+      <Product.Description />
+    </Product>
+  )
+}
+
+function Gallery() {
+  const {media} = useProduct();
+
+  return (
+    <ul>
+      {media.map((productMedia) => {
+        return (
+          <li>
+            <MediaFile media={productMedia} options={{
+              width: 100,
+              height: 100,
+              crop: 'center'
+            }}/>
+          </li>
+        )
+      })}
+    </ul>
+  );
 }
